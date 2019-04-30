949102cbc9d3c7a0fb7ec7ff205b7417dcc51e98
unknown <kpnba90@gmail.com>
2019-04-30 13:42:49 -0400

    fixed pathing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const playstores = require('./playstores');
+const playstores = require('../playstores');
 const {capitalize} = require('./helper');
 const app = express();
 
