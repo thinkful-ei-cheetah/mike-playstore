630ac9995bbdae0cac45e268d68948c88552e76b
unknown <kpnba90@gmail.com>
2019-04-30 14:12:49 -0400

    fixed

--- src/app.js
+++ src/app.js
@@ -6,35 +6,46 @@ const app = express();
 
 app.use(morgan('common'));
 
+// get pathing for req and res
 app.get('/store', (req, res) => {
   let {sort, genres} = req.query;
   let results = playstores;
 
   if (sort) {
+    // capitalize from helper file
     sort = capitalize(sort);
+    // validating if rating and app is not included to throw msg
     if (!['Rating', 'App'].includes(sort)) {
       return res
         .status(400)
         .send('Sort must be of App or Rating');
     }
+
     if (sort === 'Rating') {
       results.sort((a, b) => {
+        // if 'a' is less than 'b' return '1'
+        // if 'a' is greater than 'b' return '-1'
+        // else return '0'
         return a[sort] < b[sort] ? 1 : a[sort] > b[sort] ? -1 : 0;
       });
     } else {
       results.sort((a, b) => {
+        // same case as above except it is set to lowercase
         return a[sort].toLowerCase() > b[sort].toLowerCase() ? 1 : a[sort].toLowerCase() < b[sort].toLowerCase() ? -1 : 0;
       });
     }
   }
   
   if (genres) {
+    // capitalize from helper file
     genres = capitalize(genres);
+    // validating if genres include the list below
     if (!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genres)) {
       return res
         .status(400)
         .send('Genres must be one of Action, Puzzle, Strategy, Casual, Arcade or Card');
     }
+    // filter thru using regex to return genres
     results = playstores.filter(item => {
       let regex = new RegExp(genres, 'i');
       return regex.test(item['Genres']);
--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,9 @@
+// helper to capitalize just the first letter of a word
 const capitalize = (string) => {
+  // sets any string to all lower case
   string = string.toLowerCase();
+  // returns the first index of a word  and set it upper case
+  // add the rest of the word 
   return string[0].toUpperCase() + string.slice(1, string.length);
 };
 
